7d95fefa6524ac68f77a04060b65929b5538b31c
Cenan Horea <cenan.horea3@gmail.com>
2022-12-13 12:30:43 +0200

    WIP

---
+++ src/com/company/Grammar.java
@@ -0,0 +1,101 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Grammar {
+    Set<String> terminals;
+    Set<String> nonTerminals;
+    Map<String, List<List<String>>> productions;
+    String startSymbol;
+
+    public Grammar(String fileName) throws IOException {
+        terminals = new HashSet<>();
+        nonTerminals = new HashSet<>();
+        productions = new HashMap<>();
+
+        readFromFile(fileName);
+    }
+
+    private void readFromFile(String fileName) throws IOException {
+        //read terminals, nonTerminals, productions and startSymbol from file
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        nonTerminals.addAll(List.of(reader.readLine().split(" ")));
+        terminals.addAll(List.of(reader.readLine().split(" ")));
+        startSymbol = reader.readLine();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split("->");
+            List<String> lhs = List.of(parts[0].trim().split(" "));
+            if(lhs.size() > 1) {
+                throw new RuntimeException("Not CFG!");
+            }
+            String nonTerminal = lhs.get(0);
+            List<String> rhs = List.of(parts[1].trim().split(" "));
+            if (productions.containsKey(nonTerminal)) {
+                List<List<String>> value = productions.get(nonTerminal);
+                value.add(rhs);
+                productions.put(nonTerminal, value);
+            } else {
+                productions.put(nonTerminal, new ArrayList<>(Collections.singleton(rhs)));
+            }
+
+        }
+    }
+
+    void printTerminals() {
+        System.out.println("Terminals: " + terminals);
+    }
+
+    void printNonTerminals() {
+        System.out.println("NonTerminals: " + nonTerminals);
+    }
+
+    void printProductions() {
+        System.out.println("Productions: " + productions);
+    }
+
+    void printStartSymbol() {
+        System.out.println("StartSymbol: " + startSymbol);
+    }
+
+    public Set<String> getTerminals() {
+        return terminals;
+    }
+
+    public Set<String> getNonTerminals() {
+        return nonTerminals;
+    }
+
+    public Map<String, List<List<String>>> getProductions() {
+        return productions;
+    }
+
+    public String getStartSymbol() {
+        return startSymbol;
+    }
+
+    public Map<String, List<List<String>>> getProductionsForNonterminal(String nonTerminal) {
+        Map<String, List<List<String>>> result = new HashMap<>();
+        for (Map.Entry<String, List<List<String>>> entry : productions.entrySet()) {
+            if (entry.getKey().equals(nonTerminal)) {
+                result.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return result;
+    }
+
+    public Map<String, List<List<String>>> getProductionsContainingNonterminal(String nonTerminal) {
+        Map<String, List<List<String>>> result = new HashMap<>();
+        for (Map.Entry<String, List<List<String>>> entry : productions.entrySet()) {
+            for (List<String> rhs : entry.getValue()) {
+                if (rhs.contains(nonTerminal)) {
+                    result.put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/com/company/tests/ParserTest.java
@@ -0,0 +1,46 @@
+package com.company.tests;
+
+import com.company.Parser;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ParserTest {
+
+    private static Parser parser;
+
+    @BeforeAll
+    static void setUp() throws IOException {
+        parser = new Parser();
+    }
+
+    @Test
+    void testFirstSet() {
+        System.out.println("First set: " + parser.getFirstSet());
+
+        assert(parser.getFirstSet().containsKey("S"));
+        assert(parser.getFirstSet().containsKey("A"));
+        List<String> aList = List.of("b","c");
+        assert (parser.getFirstSet().get("A").containsAll(aList));
+        List<String> sList = List.of("a");
+        assert (parser.getFirstSet().get("S").containsAll(sList));
+
+        System.out.println("First set tests passed!");
+    }
+
+    @Test
+    void testFollowSet() {
+        System.out.println("Follow set: " + parser.getFollowSet());
+
+        assert(parser.getFollowSet().containsKey("S"));
+        assert(parser.getFollowSet().containsKey("A"));
+        List<String> aList = List.of("$");
+        assert (parser.getFollowSet().get("A").containsAll(aList));
+        List<String> sList = List.of("$");
+        assert (parser.getFollowSet().get("S").containsAll(sList));
+
+        System.out.println("Follow set tests passed!");
+    }
+}
